3eefc0b8e3c9fc7f11bda624fa80b2eb5372774b
Kayla Dailey <kayladailey000@gmail.com>
2020-04-28 16:55:36 -0500

    MVP Complete

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const secrets = require('../config/secrets')
+
+module.exports = (req, res, next) => {
+
+  try{
+const token = req.headers.authorization.split(" ")[1];
+console.log(token);
+
+
+if (token){
+  jwt.verify(token, secrets.jwt_secret, (err, decodedToken) =>{
+      if (err){
+        throw new Error(err)
+      } else  {
+        req.decodedToken = decodedToken;
+        next();
+    } 
+  })
+}else {
+throw new Error ("Bad Auth")
+  }
+} catch (err){
+  res.status(401).json(err.message);
+}
+};
